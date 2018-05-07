9948bfcfa2750a64b6216b3838d736ee29a0362f
ponyo <ponyo@adquestasia.com>
2018-05-07 19:41:10 +0700

    debug send mail 3

---
+++ dowork.php
@@ -0,0 +1,33 @@
+<?php
+require_once("PHPMailerAutoload.php");
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'zunik.web.contact@gmail.com';                 // SMTP username
+$mail->Password = 'han11235813';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->From = 'zunik.web.contact@gmail.com';
+$mail->FromName = 'Mailer';
+$mail->addAddress('ngochanvt0402@gmail.com', 'Han tran');     // Add a recipient
+
+//$mail->addReplyTo('info@example.com', 'Information');
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
